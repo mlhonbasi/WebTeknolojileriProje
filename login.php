19557e5e29e192b266c39f61de327c9df6d6b88c
mlhonbas <melihonbasi16@gmail.com>
2023-05-09 23:46:08 +0300

    login sayfasi ve genel guncelleme

--- login.php
+++ login.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mirasımız</title>
+    <title>Şehrim</title>
 
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
+
 <body>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
 
@@ -41,6 +43,19 @@
             </ul>
         </div>
     </nav>
+    <?php
+        $eposta = "g221210350@sakarya.edu.tr";
+        $sifre = "g221210350";
+        if(($_POST["email"] == $eposta) && ($_POST["passw"] == $sifre))
+        {
+            echo "<h1>Hoş Geldiniz</h1>";
+        }
+        else
+        {
+            echo "Giriş bilgileriniz hatalı!";
+        }
+        ?>
+    
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
     </script>
@@ -51,4 +66,5 @@
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
 </body>
+
 </html>
