c8ef4f02bb67baf08f45b034fc03e7390c720507
mlhonbas <melihonbasi16@gmail.com>
2023-05-21 17:21:44 +0300

    son

--- login.php
+++ login.php
@@ -6,11 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>G221210350</title>
 
+    <!--CSS, Bootstrap & Ajax JQuery-->
     <link rel="stylesheet" href="css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 </head>
 <body>
+  <!--Navbar menusu-->
 <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
         <div class="container-fluid">
           <a class="navbar-brand" href="index.html">Anasayfa</a>
@@ -41,7 +43,9 @@
           </div>
         </div>
       </nav>
-    
+  <!--Navbar menu bitisi-->
+
+  <!--PHP giris kontrol-->
     <div class="container">
     <?php
         $eposta = "g221210350@sakarya.edu.tr";
@@ -57,7 +61,9 @@
         }
     ?>
     </div>
-
+  <!--PHP-->   
+  
+    <!--Bootstrap src & Javascript src-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
